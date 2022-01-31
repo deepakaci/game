ceec3a2203a1ff594c4e35275a4d9486b3248141
deepakaci <dipakkumaraci2015@gmail.com>
2022-01-31 15:55:03 +0530

    removing swagger dependency

--- src/main/java/com/example/game/GameApplication.java
+++ src/main/java/com/example/game/GameApplication.java
@@ -1,13 +1,9 @@
 package com.example.game;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
-@EnableSwagger2
 public class GameApplication {
 
 	public static void main(String[] args) {
